824e8555ffbd9fe610e4d077d7c9b57201ae28d3
Zammystuff <62779038+Zammystuff@users.noreply.github.com>
2025-07-15 22:52:45 -0500

    Basic recipes
    Add basic recipes, like Iron and Copper bars, and Iron and Copper plates.

---
+++ init.py
@@ -0,0 +1,15 @@
+import classes
+import bars
+##"material will likely be its on class down the line. placeholder for now,same with outputs "
+
+
+
+bar_recipes = [bars.r_ironBar,bars.r_copperBar]
+
+
+constructor_recipes = []  #need a better name for this, but it includes wires, iron plates, cadmium plates, etc. 
+
+recipes_categories = {
+        "bars":bar_recipes,
+        "rudimentary":constructor_recipes    
+}
