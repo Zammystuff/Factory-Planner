968a65daaed8623fdfa7c76e914afbe6376ee4a6
Zammystuff <62779038+Zammystuff@users.noreply.github.com>
2025-07-15 22:55:06 -0500

    Delete recipes.py

--- recipes.py
+++
@@ -1,9 +0,0 @@
-import classes
-
-
-
-
-##"material will likely be its on class down the line. placeholder for now,same with outputs "
-
-r_ironBar = classes.Recipe("Iron bar","materialIn","materialOut",1.00,"file/dir/placeholder")
-r_copperBar = ("Copper bar","materialIn","materialOut",1.00,"file/dir/placeholder")
